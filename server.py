2ae6260749b25be5c09531670dc15e9770510920
Emmett Agnew <eragnew@gmail.com>
2015-11-06 11:44:16 -0800

    Added delete functionality

--- server.py
+++ server.py
@@ -7,7 +7,9 @@ team = [
     {'id': 2,'first_name': 'Michael', 'last_name': 'Jordan' , 'jersey': 23 },
     {'id': 3,'first_name': 'Larry' , 'last_name': 'Bird' , 'jersey': 33 },
     {'id': 4,'first_name': 'Shawn' , 'last_name': 'Kemp' , 'jersey': 40},
-    {'id': 5,'first_name': 'Kareem' , 'last_name': 'Abdul-Jabbar' , 'jersey': 33 }]
+    {'id': 5,'first_name': 'Kareem' , 'last_name': 'Abdul-Jabbar' , 'jersey': 33 }
+]
+
 #helper functions
 def find_player(id):
     for player in session['team']:
@@ -46,8 +48,6 @@ def edit(player_id):
 @app.route('/players/<int:player_id>', methods = ['POST'])
 def update(player_id):
     player = find_player(player_id)
-
-
     player['first_name'] = request.form['first_name']
     player['last_name'] = request.form['last_name']
     player['jersey'] = request.form['jersey']
@@ -61,7 +61,6 @@ def update(player_id):
             #player = {}
         if player_ses['id'] == player_id:
             player_ses = player
-
     print player['first_name']
     print player['last_name']
     print player['jersey']
@@ -72,7 +71,19 @@ def update(player_id):
         print "no team yet"
     return redirect('/')
 
-
+@app.route('/players/<int:player_id>/delete', methods=['POST'])
+def delete(player_id):
+	player = find_player(player_id)
+	if player:
+		for player_ses in session['team']:
+			if player_ses['id'] == player_id:
+				session['team'].remove(player_ses)
+	return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
